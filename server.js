3277873fa372bc16d2f8fb9457c887daa7e37234
Sergiy Voznyak <sergiy.voznyak@gmail.com>
2017-08-19 00:27:46 +0200

    update server headers

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ const htmlParser = require('./modules/html-parser.js');
 
 const app = express();
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.configure(() => {
+  app.use(allowCrossDomain);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
 app.get('/games/:gameId', (req, res) => {
   const gameId = req.params.gameId;
   request(`http://quest.ua/GameStat.aspx?gid=${gameId}`, (error, response, body) => {
